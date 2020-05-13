5a4129cf5940c79199f6351fb6a8c213ba712d28
Дарья <65209824+KuznecovaDarya@users.noreply.github.com>
2020-05-13 20:46:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+using MyLibrary;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(lib1.stpow("ha", 4));
+            Console.WriteLine(lib1.rev("hello"));
+            Console.WriteLine(lib1.remov("aHeallao","a"));
+            Console.WriteLine(lib1.leng("Hello, friends!"));
+        }
+    }
+}
---
+++ lib1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class lib1
+    {
+        static public string stpow(string a, int b)
+        {
+            string a1 = a;
+            for (int i = 0; i < b - 1; i++)
+            {
+                a = a + a1;
+            }
+            return (a);
+        }
+        static public string rev(string a)
+        {
+            string h = "";
+            for (int i = a.Length - 1; i > -1; i--) 
+            {
+                h = h + a[i] + "";
+            }
+            return (h);
+        }
+        static public string remov(string str, string s)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                str = str.Replace(s, "");
+            }
+            return (str);
+        }
+        static public int leng(string str)
+        {
+            int a = str.Length;
+            return (a);
+        }
+    }
+}
+
